f92dfca677c9fec62378eff99035eb59968e91b9
William Wang <william@William-MacBook.local>
2020-02-20 14:36:11 -0600

    done gathering data

--- p2/analyze.py
+++ p2/analyze.py
@@ -1,29 +1,78 @@
 import matplotlib.pyplot as plt
 
 N = 20
-X = [2, 4, 8, 16]
-X1 = [1] + X
-TYPES = {"simple", "coarse", "fine"}
+X = [1, 2, 4, 8, 16]
+X0 = X[0:len(X) - 1]
+X1 = X[1:len(X)]
+TYPES = ['simple', 'coarse', 'fine']
+FMTS = ['-ro', '-g^', '-bs']
 
 hash_times = {typ: {} for typ in TYPES}
-group_times = {typ: {} for typ in TYPES}
 comp_times = {typ: {} for typ in TYPES}
+group_mutex_times = {typ: {} for typ in TYPES}
+group_chan_times = {typ: {} for typ in TYPES}
+group_chan_k_times = {typ: {} for typ in TYPES}
 
 for typ in TYPES:
-    for x in X1:
+    for x in X:
         with open(f"output/{typ}_hash_{x}.txt", "r") as fin:
             hash_times[typ][x] = sum(int(t) for t in fin) / N
         with open(f"output/{typ}_comp_{x}.txt", "r") as fin:
             comp_times[typ][x] = sum(int(t) for t in fin) / N
+        with open(f"output/{typ}_group_{x}_{x}.txt", "r") as fin:
+            group_mutex_times[typ][x] = sum(int(t) for t in fin) / N
+        with open(f"output/{typ}_group_{x}_1.txt", "r") as fin:
+            group_chan_times[typ][x] = sum(int(t) for t in fin) / N
+        with open(f"output/{typ}_group_16_{x}.txt", "r") as fin:
+            group_chan_k_times[typ][x] = sum(int(t) for t in fin) / N
 
-plt.xlabel('threads')
+hash_seq_times = {typ: hash_times[typ][1] for typ in TYPES}
+
+plt.xlabel('hash_workers')
 plt.ylabel('speedup')
+for (typ, fmt) in zip(TYPES, FMTS):
+    plt.plot(X1, [(hash_seq_times[typ] / hash_times[typ][x])
+                  for x in X1], fmt, label=f"hash_{typ}")
+plt.legend(loc='upper left')
+plt.savefig('data/hash.png', format='png', dpi=255)
+plt.clf()
 
-plt.plot(X, [(group_times["simple"][1] / group_times["simple"][x])
-             for x in X], '-ro', label=f"group_simple")
-plt.plot(X, [(group_times["coarse"][1] / group_times["coarse"][x])
-             for x in X], '-g^', label=f"group_coarse")
-plt.plot(X, [(group_times["fine"][1] / group_times["fine"][x])
-             for x in X], '-bs', label=f"group_fine")
-plt.legend(loc="upper left")
-plt.show()
+comp_seq_times = {typ: comp_times[typ][1] for typ in TYPES}
+
+plt.xlabel('comp_workers')
+plt.ylabel('speedup')
+for (typ, fmt) in zip(TYPES, FMTS):
+    plt.plot(X1, [(comp_seq_times[typ] / comp_times[typ][x])
+                  for x in X1], fmt, label=f"comp_{typ}")
+plt.legend(loc='center right')
+plt.savefig('data/comp.png', format='png', dpi=255)
+plt.clf()
+
+group_seq_times = {typ: group_mutex_times[typ][1] for typ in TYPES}
+
+plt.xlabel('workers')
+plt.ylabel('speedup')
+for (typ, fmt) in zip(TYPES, FMTS):
+    plt.plot(X1, [(group_seq_times[typ] / group_mutex_times[typ][x])
+                  for x in X1], fmt, label=f"group_mutex_{typ}")
+plt.legend(loc='center right')
+plt.savefig('data/group_mutex.png', format='png', dpi=255)
+plt.clf()
+
+plt.xlabel('hash_workers')
+plt.ylabel('speedup')
+for (typ, fmt) in zip(TYPES, FMTS):
+    plt.plot(X1, [(group_seq_times[typ] / group_chan_times[typ][x])
+                  for x in X1], fmt, label=f"group_chan_{typ}")
+plt.legend(loc='center right')
+plt.savefig('data/group_chan.png', format='png', dpi=255)
+plt.clf()
+
+plt.xlabel('data_workers')
+plt.ylabel('speedup')
+for (typ, fmt) in zip(TYPES, FMTS):
+    plt.plot(X0, [(group_seq_times[typ] / group_chan_k_times[typ][x])
+                  for x in X0], fmt, label=f"group_chan_k_{typ}")
+plt.legend(loc='center right')
+plt.savefig('data/group_chan_k.png', format='png', dpi=255)
+plt.clf()
--- p2/workspace/run.sh
+++ p2/workspace/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-go run *.go -input="../input/coarse.txt" -hash-workers=$1 -data-workers=$2 -comp-workers=$3
+go run *.go -d -input="../input/fine.txt" -hash-workers=$1 -data-workers=$2 -comp-workers=$3
